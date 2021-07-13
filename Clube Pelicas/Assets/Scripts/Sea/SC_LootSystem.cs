7c7c842cf9522ff373d3d4eb91dcdc1e5a3b9c33
maelRicou <m.ricou@rubika-edu.com>
2021-07-13 11:57:12 +0100

    Upgrade System

--- Clube Pelicas/Assets/Scripts/Sea/SC_LootSystem.cs
+++ Clube Pelicas/Assets/Scripts/Sea/SC_LootSystem.cs
@@ -14,7 +14,9 @@ namespace Pelicas
 
     public class SC_LootSystem : MonoBehaviour
     {
-        public ItemToSpawn[] itemToSpawn;
+        public ItemToSpawn[] itemToSpawn_1;
+        public ItemToSpawn[] itemToSpawn_2;
+        public ItemToSpawn[] itemToSpawn_3;
 
         SC_LootedItemSlot lootedItem;
 
@@ -27,22 +29,61 @@ namespace Pelicas
 
         void Start()
         {
-
-            for (int i = 0; i < itemToSpawn.Length; i++)
+            if(PlayerPrefs.GetInt("hull") == 0)
             {
+                for (int i = 0; i < itemToSpawn_1.Length; i++)
+                {
 
-                if (i == 0)
-                {
-                    itemToSpawn[i].minSpawnProb = 0;
-                    itemToSpawn[i].maxSpawnProb = itemToSpawn[i].spawnRate - 1;
-                }
-                else
-                {
-                    itemToSpawn[i].minSpawnProb = itemToSpawn[i - 1].maxSpawnProb + 1;
-                    itemToSpawn[i].maxSpawnProb = itemToSpawn[i].minSpawnProb + itemToSpawn[i].spawnRate - 1;
+                    if (i == 0)
+                    {
+                        itemToSpawn_1[i].minSpawnProb = 0;
+                        itemToSpawn_1[i].maxSpawnProb = itemToSpawn_1[i].spawnRate - 1;
+                    }
+                    else
+                    {
+                        itemToSpawn_1[i].minSpawnProb = itemToSpawn_1[i - 1].maxSpawnProb + 1;
+                        itemToSpawn_1[i].maxSpawnProb = itemToSpawn_1[i].minSpawnProb + itemToSpawn_1[i].spawnRate - 1;
+                    }
                 }
             }
-            
+
+            if (PlayerPrefs.GetInt("hull") == 1)
+            {
+                for (int i = 0; i < itemToSpawn_2.Length; i++)
+                {
+
+                    if (i == 0)
+                    {
+                        itemToSpawn_2[i].minSpawnProb = 0;
+                        itemToSpawn_2[i].maxSpawnProb = itemToSpawn_2[i].spawnRate - 1;
+                    }
+                    else
+                    {
+                        itemToSpawn_2[i].minSpawnProb = itemToSpawn_2[i - 1].maxSpawnProb + 1;
+                        itemToSpawn_2[i].maxSpawnProb = itemToSpawn_2[i].minSpawnProb + itemToSpawn_2[i].spawnRate - 1;
+                    }
+                }
+            }
+
+
+            if (PlayerPrefs.GetInt("hull") == 2)
+            {
+                for (int i = 0; i < itemToSpawn_3.Length; i++)
+                {
+
+                    if (i == 0)
+                    {
+                        itemToSpawn_3[i].minSpawnProb = 0;
+                        itemToSpawn_3[i].maxSpawnProb = itemToSpawn_3[i].spawnRate - 1;
+                    }
+                    else
+                    {
+                        itemToSpawn_3[i].minSpawnProb = itemToSpawn_3[i - 1].maxSpawnProb + 1;
+                        itemToSpawn_3[i].maxSpawnProb = itemToSpawn_3[i].minSpawnProb + itemToSpawn_3[i].spawnRate - 1;
+                    }
+                }
+            }
+
 
         }
 
@@ -51,33 +92,187 @@ namespace Pelicas
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                Spawnner();
+                if(PlayerPrefs.GetInt("hull") == 0)
+                {
+                    SpawnnerHull_1();
+                }
+
+                if (PlayerPrefs.GetInt("hull") == 1)
+                {
+                    SpawnnerHull_2();
+                }
+
+                if (PlayerPrefs.GetInt("hull") == 2)
+                {
+                    SpawnnerHull_3();
+                }
+
             }
         }
 
         #endregion
 
         #region - PRIVATE_FUNCTIONS -
-        void Spawnner()
+        void SpawnnerHull_1()
         {
             float randomNum = Random.Range(0, 100);
 
-            for (int i = 0; i < itemToSpawn.Length; i++)
+            for (int i = 0; i < itemToSpawn_1.Length; i++)
             {
-                if (randomNum >= itemToSpawn[i].minSpawnProb && randomNum <= itemToSpawn[i].maxSpawnProb)
+                if (randomNum >= itemToSpawn_1[i].minSpawnProb && randomNum <= itemToSpawn_1[i].maxSpawnProb)
                 {
-                    for (int e = 0; e < lootedItem.slots.Length; e++)
+
+                    if(PlayerPrefs.GetInt("stock") == 0)
                     {
-                        if(lootedItem.isFull[e] == false)
+                        for (int e = 0; e < lootedItem.slots_1.Length; e++)
                         {
-                            Instantiate(itemToSpawn[i].item, lootedItem.slots[e].transform, false);
-                            lootedItem.isFull[e] = true;
-                            break;
+                            if (lootedItem.isFull_1[e] == false)
+                            {
+                                Instantiate(itemToSpawn_1[i].item, lootedItem.slots_1[e].transform, false);
+                                lootedItem.isFull_1[e] = true;
+                                break;
+                            }
                         }
                     }
 
-                    //Instantiate(itemToSpawn[i].item, transform.position, Quaternion.identity);
-                    //break;
+
+                    if (PlayerPrefs.GetInt("stock") == 1)
+                    {
+                        for (int e = 0; e < lootedItem.slots_2.Length; e++)
+                        {
+                            if (lootedItem.isFull_2[e] == false)
+                            {
+                                Instantiate(itemToSpawn_1[i].item, lootedItem.slots_2[e].transform, false);
+                                lootedItem.isFull_2[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+
+                    if (PlayerPrefs.GetInt("stock") == 2)
+                    {
+                        for (int e = 0; e < lootedItem.slots_3.Length; e++)
+                        {
+                            if (lootedItem.isFull_3[e] == false)
+                            {
+                                Instantiate(itemToSpawn_1[i].item, lootedItem.slots_3[e].transform, false);
+                                lootedItem.isFull_3[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        void SpawnnerHull_2()
+        {
+            float randomNum = Random.Range(0, 100);
+
+            for (int i = 0; i < itemToSpawn_2.Length; i++)
+            {
+                if (randomNum >= itemToSpawn_2[i].minSpawnProb && randomNum <= itemToSpawn_2[i].maxSpawnProb)
+                {
+
+                    if (PlayerPrefs.GetInt("stock") == 0)
+                    {
+                        for (int e = 0; e < lootedItem.slots_1.Length; e++)
+                        {
+                            if (lootedItem.isFull_1[e] == false)
+                            {
+                                Instantiate(itemToSpawn_2[i].item, lootedItem.slots_1[e].transform, false);
+                                lootedItem.isFull_1[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+
+                    if (PlayerPrefs.GetInt("stock") == 1)
+                    {
+                        for (int e = 0; e < lootedItem.slots_2.Length; e++)
+                        {
+                            if (lootedItem.isFull_2[e] == false)
+                            {
+                                Instantiate(itemToSpawn_2[i].item, lootedItem.slots_2[e].transform, false);
+                                lootedItem.isFull_2[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+
+                    if (PlayerPrefs.GetInt("stock") == 2)
+                    {
+                        for (int e = 0; e < lootedItem.slots_3.Length; e++)
+                        {
+                            if (lootedItem.isFull_3[e] == false)
+                            {
+                                Instantiate(itemToSpawn_2[i].item, lootedItem.slots_3[e].transform, false);
+                                lootedItem.isFull_3[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+
+        void SpawnnerHull_3()
+        {
+            float randomNum = Random.Range(0, 100);
+
+            for (int i = 0; i < itemToSpawn_3.Length; i++)
+            {
+                if (randomNum >= itemToSpawn_3[i].minSpawnProb && randomNum <= itemToSpawn_3[i].maxSpawnProb)
+                {
+
+                    if (PlayerPrefs.GetInt("stock") == 0)
+                    {
+                        for (int e = 0; e < lootedItem.slots_1.Length; e++)
+                        {
+                            if (lootedItem.isFull_1[e] == false)
+                            {
+                                Instantiate(itemToSpawn_3[i].item, lootedItem.slots_1[e].transform, false);
+                                lootedItem.isFull_1[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+
+                    if (PlayerPrefs.GetInt("stock") == 1)
+                    {
+                        for (int e = 0; e < lootedItem.slots_2.Length; e++)
+                        {
+                            if (lootedItem.isFull_2[e] == false)
+                            {
+                                Instantiate(itemToSpawn_3[i].item, lootedItem.slots_2[e].transform, false);
+                                lootedItem.isFull_2[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
+
+                    if (PlayerPrefs.GetInt("stock") == 2)
+                    {
+                        for (int e = 0; e < lootedItem.slots_3.Length; e++)
+                        {
+                            if (lootedItem.isFull_3[e] == false)
+                            {
+                                Instantiate(itemToSpawn_3[i].item, lootedItem.slots_3[e].transform, false);
+                                lootedItem.isFull_3[e] = true;
+                                break;
+                            }
+                        }
+                    }
+
                 }
             }
         }
--- Clube Pelicas/Assets/Scripts/Sea/SC_LootedItemSlot.cs
+++ Clube Pelicas/Assets/Scripts/Sea/SC_LootedItemSlot.cs
@@ -6,8 +6,14 @@ namespace Pelicas
 {
     public class SC_LootedItemSlot : MonoBehaviour
     {
-        public bool[] isFull;
-        public GameObject[] slots;
+        public bool[] isFull_1;
+        public GameObject[] slots_1;
+
+        public bool[] isFull_2;
+        public GameObject[] slots_2;
+
+        public bool[] isFull_3;
+        public GameObject[] slots_3;
     }
 }
 
