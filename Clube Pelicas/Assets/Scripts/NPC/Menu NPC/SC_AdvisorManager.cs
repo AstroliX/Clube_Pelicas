94f44c913e4ecdfb79a84aea21cdd8308d153024
maelRicou <m.ricou@rubika-edu.com>
2021-07-15 15:56:36 +0100

    Advisor Menu 3/2

---
+++ Clube Pelicas/Assets/Scripts/NPC/Menu NPC/SC_AdvisorManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Pelicas
+{
+    public class SC_AdvisorManager : MonoBehaviour
+    {
+
+        [Header("MENUs")]
+        [SerializeField] GameObject mainMenu;
+        [SerializeField] GameObject crewMenu;
+        [SerializeField] GameObject payCrewMenu;
+
+        [SerializeField] GameObject areYouSureUWantSea;
+
+
+        
+
+        [Space]
+        [Header("Crew Menu")]
+        [SerializeField] GameObject moneyUHave;
+        [SerializeField] GameObject crew_buttons;
+
+        #region - UNITY_FUNCTIONS -
+        #endregion
+
+        #region - PUBLIC_FUNCTIONS -
+        #endregion
+
+        #region - PRIVATE_FUNCTIONS -
+        #endregion
+    }
+}
+
